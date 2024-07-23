f95f3329b878227e2f54f98d8b8e93dc00c7d519
Dirk Peter <dirk.peter@softwareg.com>
2024-07-23 15:24:31 +0200

    adds c8y alarm icon component

---
+++ micro-components/alarm-icon/alarm-icon.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { IAlarm } from '@c8y/client';
+import { CoreModule } from '@c8y/ngx-components';
+
+@Component({
+  selector: 'c8y-alarm-icon',
+  templateUrl: './alarm-icon.component.html',
+  standalone: true,
+  imports: [CoreModule],
+})
+export class AlarmIconComponent {
+  @Input() placement: 'top' | 'right' | 'bottom' | 'left' = 'right';
+  @Input() display: 'severity' | 'status' = 'severity';
+  @Input() severity: string;
+  @Input() status: string;
+
+  @Input() set alarm(alarm: IAlarm) {
+    this.severity = String(alarm.severity);
+    this.status = String(alarm.status);
+  }
+}
