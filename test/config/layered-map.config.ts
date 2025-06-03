e6ef13a29ec92c108685e7adf1c72ff1e8234cd6
Hendrik Näther <hendrik.naether@cumulocity.com>
2025-06-03 13:50:18 +0200

    #73: Adapting to the changes from #4

--- test/config/layered-map.config.ts
+++ test/config/layered-map.config.ts
@@ -2,5 +2,5 @@ import { baseConfig } from './base.config';
 import map from '../../packages/layered-map/cumulocity.config';
 
 export default baseConfig(JSON.stringify(map.runTime.remotes), [
-  `cypress/e2e/*${map.runTime.name}*.cy.ts`,
+  `cypress/e2e/cumulocity-layered-map-widget*.cy.ts`,
 ]);
