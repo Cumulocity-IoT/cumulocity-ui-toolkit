e0c5b9bdcec53ba21fd2f3f2be93da3dceb95df2
Dirk Peter <67102395+dirk-peter-c8y@users.noreply.github.com>
2025-11-25 17:14:38 +0100

    #35 Makes time input field standalone and imports date picker

--- libs/components/_formly-fields/time.formly/time.formly.component.ts
+++ libs/components/_formly-fields/time.formly/time.formly.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { DateTimePickerModule } from '@c8y/ngx-components';
 import { FieldType, FieldTypeConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'formly-time',
   templateUrl: './time.formly.component.html',
+  standalone: true,
+  imports: [DateTimePickerModule],
 })
 export class TimeFieldType extends FieldType<FieldTypeConfig> {}
