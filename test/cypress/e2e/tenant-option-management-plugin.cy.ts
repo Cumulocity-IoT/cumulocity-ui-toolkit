9fa88cb186ffb93a87804d50e908cf045dac0163
Hendrik Näther <hendrik.naether@cumulocity.com>
2026-06-02 22:11:03 +0200

    Fixing tenant option test

--- test/cypress/e2e/tenant-option-management-plugin.cy.ts
+++ test/cypress/e2e/tenant-option-management-plugin.cy.ts
@@ -3,11 +3,13 @@
 describe('Tenant Option Management', () => {
   before(() => {
     Cypress.session.clearAllSavedSessions();
-
+  });
+  
+  beforeEach(() => {
     cy.getAuth().login().disableGainsight();
   });
 
   it('Verify proper setup', () => {
-    cy.visitShellAndWaitForSelector('', 'en', '#navigator');
+    cy.visitShellAndWaitForSelector('', 'en', 'c8y-navigator-node button[data-cy="Options"]');
   });
 });
