d1a476a88cb15e7d8e525d2dfbcc5f520d7b4c18
Christian Guether <christian.guether@cumulocity.com>
2026-08-25 15:22:24 +0200

    test(favorites-manager): assert search-in-list nav icon (#81)

--- test/cypress/e2e/favorites-manager-plugin.cy.ts
+++ test/cypress/e2e/favorites-manager-plugin.cy.ts
@@ -24,4 +24,20 @@ describe('Favorites Manager', () => {
     // expect the favorites list component to be visible
     cy.get('c8y-favorites-manager').should('exist').should('be.visible');
   });
+
+  it('should render the "search-in-list" icon for the Favorites navigator entry (#81)', () => {
+    // open the Cockpit application extended with the Favorites Manager module locally
+    // and wait for the navigator menu to be visible
+    cy.visitShellAndWaitForSelector('', 'en', '#navigator');
+
+    // the navigator icon lives as a sibling of the entry's button, inside the shared
+    // .link wrapper rendered by c8y-navigator-node - assert it uses the fixed icon
+    // (previously 'mark-as-favorite', now 'search-in-list' per issue #81)
+    cy.get('c8y-navigator-node button[data-cy="favorites.title"]', { timeout: 60000 })
+      .should('exist')
+      .parents('.link')
+      .find('c8y-navigator-icon i')
+      .should('have.class', 'dlt-c8y-icon-search-in-list')
+      .and('not.have.class', 'dlt-c8y-icon-mark-as-favorite');
+  });
 });
